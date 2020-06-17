09513afca85e1f52d1d4ef4924947310f92dd674
Ryan Antonelli <ryan.a.antonelli@gmail.com>
2020-06-17 13:07:33 -0700

    Updates

---
+++ precache-manifest.b69547c6f82e63528a3ab708b6dd5947.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f553ddd8064fc5d48a48780cc1306804",
+    "url": "/NewPortfolio/index.html"
+  },
+  {
+    "revision": "2297720fae3593d8b152",
+    "url": "/NewPortfolio/static/js/2.7d776539.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/NewPortfolio/static/js/2.7d776539.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29e7377ba0b346c8fdc0",
+    "url": "/NewPortfolio/static/js/main.432765e2.chunk.js"
+  },
+  {
+    "revision": "700e14a82f5c86effa59",
+    "url": "/NewPortfolio/static/js/runtime-main.48f093ea.js"
+  }
+]);
